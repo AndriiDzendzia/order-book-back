ae17a6983bd2347a85030dd9cd46485b5cd64f90
DzendziaAndrii <andriidzendzia@gmail.com>
2023-04-09 00:29:20 +0300

    Added AddOrderBookCommand

---
+++ OrderBook/OrderBook/Handlers/AddOrderBookCommand.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="AddOrderBookCommand.cs" company="AndriiDzendzia">
+// Copyright (c) AndriiDzendzia. All rights reserved.
+// </copyright>
+
+using MediatR;
+using OrderBook.DAL.Model;
+using OrderBook.DTOs;
+using OrderBook.HttpClients;
+
+namespace OrderBook.Handlers
+{
+    public class AddOrderBookCommand : IRequest<Result<DAL.Model.OrderBook>>
+    {
+        public string? CurrencyPair { get; set; } = "btceur";
+
+        public class Handler : IRequestHandler<AddOrderBookCommand, Result<DAL.Model.OrderBook>>
+        {
+            private readonly IBitstampClient _bitstampClient;
+            private readonly OrderBookContext _orderBookContext;
+
+            public Handler(OrderBookContext orderBookContext, IBitstampClient bitstampClient)
+            {
+                _orderBookContext = orderBookContext;
+                _bitstampClient = bitstampClient;
+            }
+
+            public async Task<Result<DAL.Model.OrderBook>> Handle(AddOrderBookCommand request, CancellationToken cancellationToken)
+            {
+                Result<BitstampOrderBookDto> result = await _bitstampClient.GetOrderBook(request.CurrencyPair!);
+
+                if (result.IsFailure)
+                {
+                    return Result<DAL.Model.OrderBook>.Failure(result.ErrorMessage, result.ErrorCode);
+                }
+
+                var orderBook = new DAL.Model.OrderBook
+                {
+                    Asks = result.Data!.Asks.Select(ask => new Order(ask)).ToList(),
+                    Bids = result.Data!.Bids.Select(bid => new Order(bid)).ToList(),
+                    Timestamp = DateTime.UtcNow,
+                    CurrencyPair = request.CurrencyPair!,
+                };
+
+                await _orderBookContext.AddAsync(orderBook, cancellationToken);
+                await _orderBookContext.SaveChangesAsync(cancellationToken);
+
+                return Result.Success(orderBook);
+            }
+        }
+    }
+}
