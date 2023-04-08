3ce50c5a10d8fa5bdb97463b5d4f1ea840da9bdf
DzendziaAndrii <andriidzendzia@gmail.com>
2023-04-08 20:20:24 +0300

    Project setup

---
+++ OrderBook/OrderBook.DAL/Model/OrderBook.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="OrderBook.cs" company="AndriiDzendzia">
+// Copyright (c) AndriiDzendzia. All rights reserved.
+// </copyright>
+
+namespace OrderBook.DAL.Model
+{
+    public class OrderBook
+    {
+
+    }
+}
---
+++ OrderBook/OrderBook/Controllers/OrderBookController.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="OrderBookController.cs" company="AndriiDzendzia">
+// Copyright (c) AndriiDzendzia. All rights reserved.
+// </copyright>
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderBookController : ControllerBase
+    {
+    }
+}
