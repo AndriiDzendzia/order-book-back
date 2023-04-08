01dee4de5d51d72251223becc39f91026a642250
DzendziaAndrii <andriidzendzia@gmail.com>
2023-04-09 00:30:11 +0300

    Added GetOrderBookFromDBQuery

---
+++ OrderBook/OrderBook/Handlers/GetOrderBookFromDBQuery.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="GetOrderBookFromDBQuery.cs" company="AndriiDzendzia">
+// Copyright (c) AndriiDzendzia. All rights reserved.
+// </copyright>
+
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OrderBook.DAL.Model;
+using OrderBook.DTOs;
+
+namespace OrderBook.Handlers
+{
+    public class GetOrderBookFromDBQuery : IRequest<Result<DAL.Model.OrderBook>>
+    {
+        public DateTime Timestamp { get; set; }
+
+        public string? CurrencyPair { get; set; } = "btceur";
+
+        public class Handler : IRequestHandler<GetOrderBookFromDBQuery, Result<DAL.Model.OrderBook>>
+        {
+            private readonly OrderBookContext _orderBookContext;
+
+            public Handler(OrderBookContext orderBookContext)
+            {
+                _orderBookContext = orderBookContext;
+            }
+
+            public async Task<Result<DAL.Model.OrderBook>> Handle(GetOrderBookFromDBQuery request, CancellationToken cancellationToken)
+            {
+                DAL.Model.OrderBook? orderBook = await _orderBookContext.OrderBooks
+                    .FirstOrDefaultAsync(
+                        o => o.CurrencyPair == request.CurrencyPair && o.Timestamp == request.Timestamp,
+                        cancellationToken);
+
+                if (orderBook is null)
+                {
+                    return Result<DAL.Model.OrderBook>.Failure("No data found");
+                }
+
+                return Result.Success(orderBook);
+            }
+        }
+    }
+}
